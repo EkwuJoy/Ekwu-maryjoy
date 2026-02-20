f4bd926e7ec0c104441c992da7ed89deeaf18324
EkwuJoy <ekwumary6@gmail.com>
2026-02-20 07:04:35 -0800

    initialize error fixed

--- src/Component/Productcard.tsx
+++ src/Component/Productcard.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import img1 from "../assets/Frame 6.png"
-import { title } from 'process'
+// import React from 'react'
+// import img1 from "../assets/Frame 6.png"
+// import { title } from 
 
-function Productcard() {
-  return (
-    <div>
-        <div>
-            <img src={title.} alt="product Image" />
-        </div>
-        <div>
-            <p>Title</p>
-            <div>Rating</div>
-        </div>
-        <div>
-            <p>subtitle</p>
-            <p>data</p>
-            <p>price</p>
-        </div>
-    </div>
-  )
-}
+// function Productcard() {
+//   return (
+//     <div>
+//         <div>
+//             <img src={title.} alt="product Image" />
+//         </div>
+//         <div>
+//             <p>Title</p>
+//             <div>Rating</div>
+//         </div>
+//         <div>
+//             <p>subtitle</p>
+//             <p>data</p>
+//             <p>price</p>
+//         </div>
+//     </div>
+//   )
+// }
 
-export default Productcard
+// export default Productcardg
